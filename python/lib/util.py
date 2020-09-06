20f991776fa2f07ff76a5c6c13c705c08a83cba9
Gerard Lanois <gerard@lanois.com>
2020-09-06 14:03:38 -0700

    Add Stack class to lib.util.

--- python/lib/util.py
+++ python/lib/util.py
@@ -1,7 +1,24 @@
 """ Convenient utility functions. """
 
 # Tell pydoc to only document these functions:
-__all__ = [ 'range_inclusive' ]
+__all__ = [ 'range_inclusive', 'Stack' ]
+
+
+class Stack:
+    def __init__(self):
+        self._stack = []
+
+    def push(self, x):
+        self._stack.append(x)
+
+    def pop(self):
+        return self._stack.pop()
+
+    def top(self):
+        return self._stack[-1]
+
+    def empty(self):
+        return len(self._stack) == 0
 
 
 def range_inclusive(*args):
@@ -27,6 +44,25 @@ def range_inclusive(*args):
 
 
 import unittest
+
+class TestStack(unittest.TestCase):
+    def test_misc(self):
+        s = Stack()
+
+        for i in range(14):
+            s.push(i)
+
+        for i in reversed(range(14)):
+            self.assertEqual(s.top(), i)
+            t = s.pop()
+            self.assertEqual(t, i)
+
+        self.assertTrue(s.empty())
+
+    def test_empty_pop(self):
+        # pop() of an empty stack throws IndexError.
+        s = Stack()
+        self.assertRaises(IndexError, s.pop)
 
 class TestRangeInclusive(unittest.TestCase):
     def test_arguments(self):
