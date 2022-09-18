f8bcc8bf155f86ffd235bd0a899af4a8fce74e55
Gerard Lanois <gerard@lanois.com>
2022-09-17 17:14:32 -0700

    Add help text to the preface block.

--- python/ppt/find.py
+++ python/ppt/find.py
@@ -1,11 +1,21 @@
 """ find - Find files recursively and optionally filter with regular expression.
 
+usage: find.py [-h] [-a] path [regex ...]
+
+positional arguments:
+  path        Directory path to search.
+  regex       Optional regular expression to match on.
+
+options:
+  -h, --help  show this help message and exit
+  -a, --all   Include hidden files and directories.
 """
 
 import argparse
 import sys
 import os
 import re
+
 
 def find(path, aall, regex):
     for root, dirs, files in os.walk(path):
@@ -25,12 +35,14 @@ def find(path, aall, regex):
                 if re.search(regex[0], os.path.join(root, f)):
                     print(os.path.join(root, f))
 
+                    
 def main(options):
     if not os.path.isdir(options.path):
         print('ERROR: %s is not a directory' % options.path())
     else:
         find(options.path, options.aall, options.regex)
 
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
