717222aec20c8398e11b9aca97d338fdf7407f92
Gerard Lanois <gerard@lanois.com>
2022-10-16 14:16:07 -0700

    Finish moving unit tests to their own separate file.

--- python/lib/util.py
+++ python/lib/util.py
@@ -37,35 +37,3 @@ def range_inclusive(*args):
     while i <= stop:
         yield i
         i += step
-
-
-import unittest
-
-class TestStack(unittest.TestCase):
-    def test_misc(self):
-        s = Stack()
-
-        for i in range(14):
-            s.push(i)
-
-        for i in reversed(range(14)):
-            self.assertEqual(s.top(), i)
-            t = s.pop()
-            self.assertEqual(t, i)
-
-        self.assertTrue(s.empty())
-
-    def test_empty_pop(self):
-        # pop() of an empty stack throws IndexError.
-        s = Stack()
-        self.assertRaises(IndexError, s.pop)
-
-class TestRangeInclusive(unittest.TestCase):
-    def test_arguments(self):
-        with self.assertRaises(TypeError):
-            list(range_inclusive())
-
-        for i in range(100):
-            self.assertEqual(list(range_inclusive(i)), list(range(i+1)))
-            self.assertEqual(list(range_inclusive(0, i)), list(range(0, i+1)))
-            self.assertEqual(list(range_inclusive(0, i, 1)), list(range(0, i+1, 1)))
