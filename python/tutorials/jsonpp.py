64a0b6db595fbb2a91a50cb4fd767c66a8b4ddc0
Gerard Lanois <gerard@lanois.com>
2024-04-18 22:18:58 -0700

    Add option to sort or not.

--- python/tutorials/jsonpp.py
+++ python/tutorials/jsonpp.py
@@ -3,23 +3,30 @@ import argparse
 import sys
 import json
 
-def pp(f):
+def pp(f, sort):
     j = json.load(f)
-    print(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': ')))
+    print(json.dumps(j, sort_keys=sort, indent=4, separators=(',', ': ')))
 
 def main(options):
     if not options.filename:
         # No filename given on the command line.
         # Process data directly from stdin.
-        pp(sys.stdin)
+        pp(sys.stdin, options.sort)
     else:
         # Read from the file.
         with open(options.filename, 'r') as f:
-            pp(f)
+            pp(f, options.sort)
     return 0
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s',
+        '--sort',
+        dest='sort',
+        help='Sort the keys..',
+        action='store_true',
+        default=False)
     parser.add_argument(
         'filename',
         help='Name of file to read.',
